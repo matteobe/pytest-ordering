95338706b0bb84670eca0fe4969151b564d15800
Matteo Berchier <maberchier@gmail.com>
2021-02-21 20:17:01 +0100

    Strategy for creating ordered execution list from graph, if no cycle and priorities conflicts present

--- pytest_ordering/directedgraph.py
+++ pytest_ordering/directedgraph.py
@@ -3,6 +3,39 @@ DirectedGraph implements the capability to reason about order of execution of it
 Cardinal ordering information (first, second, ..., second-to-last, last) can be integrated in the graph
 """
 
+# To deal with the starting conditions (first, second, third, etc..)
+# They can be added as the root of the tree
+# TODO: Decide where to attach the end of the first-priority execution list to the graph of before/after relationships
+# - Case 1: top10 priorities is a separate graph, in which case it gets executed first
+# - Case 2: top10 priorities is attached to rest of graph, in which case the normal algorithm is applied
+
+# To deal with the ending conditions (last, second-to-last, etc...)
+# They can be added as a leaf of the tree
+# TODO: Decide where to attach the start of the last-priority execution list to the graph of before/after relationships
+# - Case 1: bottom10 priorities is a separate graph, in which case it gets executed last
+# - Case 2: bottom10 priorities is attached to rest of graph, in which case the normal algorithm is applied
+
+# TODO: How do you construct the execution list
+# Apply topological sort algorithm (https://personal.utdallas.edu/~ravip/cs3345/slidesweb/node7.html)
+# Step in procedure
+# - Identify vertex without incoming edges
+# - Add vertex to reversed_execution list
+# - Remove the vertex from the graph (including incoming edges)
+# - Repeat procedure for rest of graph
+#
+# Procedure order
+# - Begin with bottom10 vertices
+# - Perform on rest of graph (excluding top10 vertices)
+# - Conclude with top10 vertices
+
+# TODO: Outstanding problem
+# - Identify if there is a priority conflict for the top10 vertices
+#   Approach: Create the "source" tree for the nr. 10 vertex and if there are vertices other than the top 10, then
+#             there is a priority conflict
+# - Identify if there is a priority conflict for the bottom10 vertices
+#   Approach: Reverse graphs direction of edge and perform the same procedure as for the nr. 10 vertex, this time
+#             starting with the nr. 10-to-last vertex
+
 from typing import Union
 from collections import defaultdict
 
@@ -18,6 +51,7 @@ class DirectedGraph:
         self.vertices_map = {}
         self.vertices_map_inv = {}
         self.graph = defaultdict(list)
+        self.graph_inv = defaultdict(list)
 
     def add_vertex(self, vertex: Union[str, int, float]) -> int:
         """
@@ -43,14 +77,19 @@ class DirectedGraph:
             vertex_id = self.vertices_map.pop(vertex)
             _ = self.vertices_map_inv.pop(vertex_id)
 
-            # Remove vertex as starting point from graph only if it was used
+            # Remove vertex as starting point from graph & inverted graph only if it was used
             if vertex_id in self.graph:
                 _ = self.graph.pop(vertex_id)
+            if vertex_id in self.graph_inv:
+                _ = self.graph_inv.pop(vertex_id)
 
-            # Remove vertex as end point from graph only if it was used
+            # Remove vertex as end point from graph & inverted graph only if it was used
             for start_vertex_id in self.graph:
                 if vertex_id in self.graph[start_vertex_id]:
                     self.graph[start_vertex_id].remove(vertex_id)
+            for start_vertex_id in self.graph_inv:
+                if vertex_id in self.graph_inv[start_vertex_id]:
+                    self.graph_inv[start_vertex_id].remove(vertex_id)
 
     def get_vertex_id(self, vertex: Union[str, int, float]) -> int:
         """
@@ -70,6 +109,7 @@ class DirectedGraph:
         start_vertex = self.get_vertex_id(start_vertex)
         end_vertex = self.get_vertex_id(end_vertex)
         self.graph[start_vertex].append(end_vertex)
+        self.graph_inv[end_vertex].append(start_vertex)
 
     def remove_edge(self, start_vertex: Union[str, int, float], end_vertex: Union[str, int, float]) -> None:
         """
@@ -81,6 +121,7 @@ class DirectedGraph:
         if start_vertex in self.graph:
             if end_vertex in self.graph[start_vertex]:
                 self.graph[start_vertex].remove(end_vertex)
+                self.graph_inv[end_vertex].remove(start_vertex)
 
     def is_cyclic(self, vertex_id, vertex_visited, recursion_stack):
         """
